d4acaf868e5518571f5e1fdbdd04dfa749849ecb
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-07-11 18:30:16 +0530

    Check given number is even or odd using java code.

---
+++ Simple/OddEven/src/com/hardik/javase/OddEven.java
@@ -0,0 +1,21 @@
+package com.hardik.javase;
+
+/**
+ * Java Odd and Even Number Example
+ * 
+ * @author HARDIK
+ *
+ */
+public class OddEven {
+
+	public static void main(String[] args) {
+
+		int no = 123;
+
+		if (no % 2 == 0) {
+			System.out.println(no + " number is even.");
+		} else {
+			System.out.println(no + " number is odd.");
+		}
+	}
+}
